b8a036c706275932e7220b0ea7f6cf5232e415ca
rohandevtown <rohankinnal@devtown.in>
2024-04-29 19:07:47 +0530

    done

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const htmlTaskContent = ({id, title, description, type, url}) =>
 `<div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
         <div class="card shadow-sm task__card">
             <div class="card-header d-flex justify-content-end task__card__header gap-2">
-                <button type="button" class="btn btn-outline-primary mr-2" name=${id}>
+                <button type="button" class="btn btn-outline-primary mr-2" name=${id} onclick="editTask.apply(this, arguments)">
                     <i class="fa-solid fa-pencil" name=${id}></i>
                 </button>
                  <button type="button" class="btn btn-outline-danger mr-2" name=${id} onclick="deleteTask.apply(this, arguments)">
@@ -166,4 +166,87 @@ const searchTask = (e) => {
        resultData.map((cardData) => {
          taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardData))
        })
+}
+   
+
+const editTask = (e) => {
+       if(!e) e = window.event;
+
+       const targetId = e.target.id;
+       const type = e.target.tagName;
+       
+       let parentNode;
+       let taskTitle;
+       let taskDescription;
+       let taskType;
+       let submitButton;
+
+       if(type === "BUTTON"){
+        parentNode = e.target.parentNode.parentNode;
+       }else{
+         parentNode = e.target.parentNode.parentNode.parentNode;
+       }
+
+       taskTitle = parentNode.childNodes[3].childNodes[3];
+    //    console.log(taskTitle);
+       taskDescription = parentNode.childNodes[3].childNodes[5];
+    //    console.log(taskDescription)
+       taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    //    console.log(taskType);
+    submitButton = parentNode.childNodes[5].childNodes[1];
+
+    taskTitle.setAttribute ("contenteditable", "true");
+    taskDescription.setAttribute ("contenteditable", "true");
+    taskType.setAttribute ("contenteditable", "true");
+
+    submitButton.setAttribute('onclick', "saveEdit.apply(this, arguments)");
+    submitButton.removeAttribute("data-bs-toggle");
+    submitButton.removeAttribute("data-bs-target");
+    submitButton.innerHTML = "Save Changes"
+}   
+
+
+const saveEdit = (e) => {
+       if(!e) e = window.event;
+
+       const targetId = e.target.id;
+       const parentNode = e.target.parentNode.parentNode;
+    //    console.log(parentNode.childNodes)
+
+    const taskTitle = parentNode.childNodes[3].childNodes[3];
+    const taskDescription = parentNode.childNodes[3].childNodes[5];
+    const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    const submitButton = parentNode.childNodes[5].childNodes[1];
+
+    const updateData = {
+        taskTitle : taskTitle.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+        taskType: taskType.innerHTML
+    }
+
+    let stateCopy = state.taskList;
+
+    stateCopy = stateCopy.map((task)=>
+        task.id === targetId ? {
+            ...task,
+            id: task.id,
+            title: updateData.taskTitle,
+            description: updateData.taskDescription,
+            type: updateData.taskType,
+            
+        } : task
+    );
+
+    state.taskList = stateCopy;
+    updateLocalStorage();
+
+    taskTitle.setAttribute ("contenteditable", "false");
+    taskDescription.setAttribute ("contenteditable", "false");
+    taskType.setAttribute ("contenteditable", "false");
+
+    
+    submitButton.setAttribute('onclick', "openTask.apply(this, arguments)");
+    submitButton.setAttribute("data-bs-toggle", "modal");
+    submitButton.setAttribute("data-bs-target", "#showTask");
+    submitButton.innerHTML = "Open Task"
 }
